933a34a96bd669ea944455dcb261222a23f524ab
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-23 15:46:28 +0500

    function is completed

--- index-counter/script.js
+++ index-counter/script.js
@@ -1,5 +1,11 @@
 var text = prompt("Enter the text:");
 
 function indexsum(text) {
-    
+    var sum = 0;
+
+    for (var i = 0; i < text.length; i++) {
+        sum += i;
+
+    }
+    return sum;
 }
