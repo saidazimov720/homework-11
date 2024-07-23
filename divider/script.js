42b27fa704a0d853b08b049d173e02e59e6ab2c7
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-23 19:58:11 +0500

    some fix

--- divider/script.js
+++ divider/script.js
@@ -4,7 +4,7 @@ for (var i = 0; i < 301; i++) {
     c.push(i);
 }
 
-for (var i = 0; i < c.length; i++) {
+for (var i = 0; i  < c.length; i++) {
     c[i] = 'someone';
 }
 
@@ -17,5 +17,6 @@ for (var i = 0; i < c.length; i++) {
     }
 }
 
-console.log(indexes);
 console.log(c);
+console.log(indexes);
+
