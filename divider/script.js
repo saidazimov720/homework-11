684c597e481590f6d7df1b322888d9c56cb848fe
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-07-24 17:45:14 +0500

    fix the bugs

--- divider/script.js
+++ divider/script.js
@@ -4,17 +4,17 @@ for (var i = 0; i < 301; i++) {
     c.push(i);
 }
 
-var indexes = [];
+var d = [];
 
 for (var i = 0; i < c.length; i ++) {
     c[i] = `someone${i}`;
-    indexes.push(i);
+    d.push(i);
 }
 
 console.log(c);
 
-for (var i = 0; i < indexes.length; i++) {
-    c[indexes[i]] = indexes[i];
+for (var i = 0; i < d.length; i++) {
+    c[d[i]] = d[i];
 }
 
-console.log(indexes);
+console.log(d);
